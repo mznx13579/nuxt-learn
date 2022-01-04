a73f603ebb6f6b4f5cc2cd6fa9b2fa0204f4a7fe
Henry <jeongsy@gabia.com>
2022-01-04 18:32:38 +0900

    add: 404 not found page

---
+++ layouts/error.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>error page</h1>
+    <p>page error currupt</p>
+    <p>page not found 404</p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
